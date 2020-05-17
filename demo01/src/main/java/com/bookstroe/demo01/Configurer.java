499b499603412d59daf3b3d9afda95fc0b38d414
world <yuanzhangzcc@163.com>
2020-05-17 14:34:32 +0800

    修复了映射图片路径问题

--- demo01/src/main/java/com/bookstroe/demo01/Configurer.java
+++ demo01/src/main/java/com/bookstroe/demo01/Configurer.java
@@ -8,21 +8,28 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
+import java.io.IOException;
 
 @Configuration
 public class Configurer implements WebMvcConfigurer {
 
-//    //映射本地图片
+    //映射本地图片路径
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/**").addResourceLocations("file:C:/Users/world/Desktop/javaweb/demo01/src/main/resources/static/img/");
+        File directory = new File("../../resources/static/img/");
+        try{
+            registry.addResourceHandler("/img/**").addResourceLocations("file:"+directory.getCanonicalPath());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     //修改上传文件大小
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        //指定文件代销
+        //指定文件大小
         factory.setMaxFileSize(DataSize.parse("5MB"));
         /// 设定上传文件大小
         factory.setMaxRequestSize(DataSize.parse("100MB"));
